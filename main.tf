d3fbb5e7cb6e8a4d82b5bb956c09b6db9c58896b
kato <katofw42@gmail.com>
2025-06-04 01:18:12 +0900

    1vm

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0-beta2"
+    }
+  }
+}
 
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "web" {
+  count         = 1
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "web-${count.index + 1}"
+  }
+}
