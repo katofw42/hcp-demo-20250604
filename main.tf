fd9abf618d92287450a9e8119335afc606978ddf
kato <katofw42@gmail.com>
2025-06-04 01:27:38 +0900

    revert

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  count         = 4
+  count         = 1
   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   instance_type = "t2.micro"
   
